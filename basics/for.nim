64f02b28ba54302e84b0e268cf47d8d57cbb2dde
bussury <bussury.omary@gmail.com>
2023-10-10 18:30:04 +0300

    app:nim basics commdand" ;

---
+++ basics/for.nim
@@ -0,0 +1,14 @@
+for i in 5..9:
+    echo i
+
+echo ""
+
+for x in countup(3,17,3):
+    echo x
+    
+echo ""
+
+let name = "Bussury Omary"
+
+for letter in name:
+    echo letter
---
+++ basics/hello.nim
@@ -0,0 +1,11 @@
+echo "Welcome Mr. Bussury"
+
+let
+    a = 11
+    b=4
+echo "a + b = ", a + b 
+echo "a - b = ", a - b 
+echo "a * b = ", a * b 
+echo "a / b = ", a / b 
+echo "a div b = ", a div b 
+echo "a mod b = ", a mod b
