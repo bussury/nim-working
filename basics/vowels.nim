1c75e5707cd16a65697725ebfa84279ef09d7dd8
bussury <bussury.omary@gmail.com>
2023-10-11 07:36:29 +0300

    app:while

---
+++ basics/vowels.nim
@@ -0,0 +1,15 @@
+var name = "Bussury Omary"
+
+for vowel in name:
+    case vowel
+    of 'a':
+        echo " a"
+    of 'e':
+        echo " e"
+    of 'i':
+        echo " i"
+    of 'o':
+        echo " o"
+    of 'u':
+        echo " u"
+    else: discard
